b68b861aed7a1935ba4caf46474d4bd647415ae1
Dev-Maradona <maradona.0000@yahoo.com>
2022-07-29 20:37:54 -0700

    Added Mega Menu

--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -1,4 +1,6 @@
 import styles from '../../styles/navbar.module.css';
+import Image from 'next/image';
+import MegaMenuImg from '../../assets/images/megamenu.png';
 
 const Navbar = () => {
     return (
@@ -19,6 +21,47 @@ const Navbar = () => {
                     </li>
                     <li>
                         <a href="#">Other links</a>
+                        {/* Start Mega Menu */}
+                        <div className={styles.mega}>
+                            <div className={styles.image}>
+                                <Image src={MegaMenuImg} alt="Mega Menu" />
+                            </div>
+                            <ul className={styles.links}>
+                                <li>
+                                    <a href="#testimonials"><i className="far fa-comments fa-fw"></i> Testimonials</a>
+                                </li>
+                                <li>
+                                    <a href="#team"><i className="far fa-user fa-fw"></i> Team Members</a>
+                                </li>
+                                <li>
+                                    <a href="#services"><i className="far fa-building fa-fw"></i> Services</a>
+                                </li>
+                                <li>
+                                    <a href="#our-skills"><i className="far fa-check-circle fa-fw"></i> Our Skills</a>
+                                </li>
+                                <li>
+                                    <a href="#work-steps"><i className="far fa-clipboard fa-fw"></i> How It Works</a>
+                                </li>
+                            </ul>
+                            <ul className={styles.links}>
+                                <li>
+                                    <a href="#events"><i className="far fa-calendar-alt fa-fw"></i> Events</a>
+                                </li>
+                                <li>
+                                    <a href="#pricing"><i className="fas fa-server fa-fw"></i> Pricing Plans</a>
+                                </li>
+                                <li>
+                                    <a href="#video"><i className="far fa-play-circle fa-fw"></i> Top Videos</a>
+                                </li>
+                                <li>
+                                    <a href="#stats"><i className="far fa-chart-bar fa-fw"></i> Stats</a>
+                                </li>
+                                <li>
+                                    <a href="#discount"><i className="fas fa-percent fa-fw"></i> Request A Discount</a>
+                                </li>
+                            </ul>
+                        </div>
+                        {/* End Mega Menu */}
                     </li>
                 </ul>
             </div>
---
+++ components/title/title.jsx
@@ -0,0 +1,11 @@
+import styles from '../../styles/title.module.css';
+
+const Title = () => {
+    return (
+        <div>
+            Title
+        </div>
+    )
+}
+
+export default Title;
